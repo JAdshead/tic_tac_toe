bb80785f0c5ae1568e1f44662df2dfe4484efb01
Jonny Adshead <jonny.adshead@gmail.com>
2015-09-18 15:32:21 +0100

    Game board basic functions. Specs passing

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,55 @@
 class Board
   attr_reader :grid
 
-  def initialize(grid = Array.new(3, Array.new(3,' ')) )
-    @grid = grid
+  def initialize( rows = 3, columns = 3, default_value = ' ' )
+    @grid          = new_grid(rows, columns, default_value)
+    @default_value = default_value
+    @rows          = rows
+    @columns       = columns
+  end
+
+  def set_cell(cell_num, value)
+    row, column = human_to_grid(cell_num)
+    @grid[row][column] = value if @grid[row][column] == @default_value
+  end
+
+  def get_cell(cell_num)
+    row, column = human_to_grid(cell_num)
+    return @grid[row][column]
+  end
+
+  # this should probably be private
+  def human_to_grid(cell_num)
+    # ensure cell_num is int & zero base the number
+    cell_num = cell_num.to_i
+    cell_num -= 1
+
+    # divide the cell_num by the number of rows to get the row
+    row = cell_num / @rows
+
+    # take the number of cells on lower rows from the cell number
+    # to get column number
+    column = cell_num - (row * @columns)
+
+    return [row, column]
+  end
+
+  def print
+    to_print = @grid.dup
+
+    while to_print.length > 1 do
+      row = to_print.shift
+      puts row.flatten.join('|')
+      puts '- - -'
+    end
+    row = to_print.shift
+    puts row.flatten.join('|')
+  end
+
+  private
+
+  def new_grid(rows, columns, cell_value)
+    Array.new(rows) { Array.new(columns, cell_value) }
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -6,28 +6,69 @@ describe Board do
   let(:empty_grid) { [[' ',' ',' '],[' ', ' ',' '],[' ',' ',' ']] }
   let(:populated_grid) { [['X','O','X'],['O','O','O'],['X','X','X']] }
 
-  it 'initializes with empty 3x3 grid with empty cells' do
+  it 'initializes with 3x3 \' \' grid when no values given' do
     expect(board.grid).to eq(empty_grid)
   end
 
-  it 'initializes with given grid' do
-    grid = [[' ', ' '],[' ', ' ']]
-    board = Board.new(grid)
+  it 'initializes with given rows and columns' do
+    grid = [[' ', ' ', ' '],[' ',' ', ' ']]
+    board = Board.new(2,3)
     expect(board.grid).to eq(grid)
   end
 
-
-
   describe '#human_to_grid' do
+    it 'takes human cell number, returns row and colunn' do
+      expect(board.human_to_grid(7)).to eq([2,0])
+    end
+
+    it 'handles int as string' do
+      expect(board.human_to_grid('2')).to eq([0,1])
+    end
+
+    context 'with a 5x5 (non default) grid' do
+      board = Board.new(5,5)
+
+      it 'takes human cell number, returns row and column' do
+        expect(board.human_to_grid(12)).to eq([2,1])
+      end
+    end
   end
 
   describe '#set_cell' do
-    it 'updates cell'
-    it 'does not write over existing value'
+    it 'updates cell' do
+      board.set_cell(3,'X')
+      board.set_cell(3,'X')
+      expect(board.grid[0][2]).to eq('X')
+    end
+
+    it 'does not write over existing value' do
+      board.set_cell(3,'X')
+      board.set_cell(3,'O')
+      expect(board.grid[0][2]).to eq('X')
+    end
   end
 
   describe '#get_cell' do
-    it 'returns cell value'
+    it 'returns cell value' do
+      board.set_cell(3,'X')
+      expect(board.get_cell(3)).to eq('X')
+    end
+  end
+
+  describe '#print' do
+    it 'prints to STDOUT' do
+      expect(STDOUT).to receive(:puts).exactly(5).times
+      board.print
+    end
+
+    context 'with a 5x5 (non default) grid' do
+      board = Board.new(5,5)
+      it 'prints to STDOUT' do
+        expect(STDOUT).to receive(:puts).exactly(9).times
+        board.print
+      end
+    end
+
   end
 
 end
