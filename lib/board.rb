a549b4b9efe1df047608e7d728b006ced0b3e438
Jonny Adshead <jonny.adshead@gmail.com>
2015-09-18 11:04:43 +0100

    WIP - Basic initialization of board object

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,8 @@
 class Board
   attr_reader :grid
 
-  def initialize
-    @grid = Array.new(3, Array.new(3,' '))
+  def initialize(grid = Array.new(3, Array.new(3,' ')) )
+    @grid = grid
   end
-
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,14 +4,30 @@ require_relative '../lib/board'
 describe Board do
   subject(:board) { Board.new }
   let(:empty_grid) { [[' ',' ',' '],[' ', ' ',' '],[' ',' ',' ']] }
+  let(:populated_grid) { [['X','O','X'],['O','O','O'],['X','X','X']] }
 
   it 'initializes with empty 3x3 grid with empty cells' do
     expect(board.grid).to eq(empty_grid)
   end
 
-  describe 'Board#setCell' do
-    it 'updates cell' do
-
-    end
+  it 'initializes with given grid' do
+    grid = [[' ', ' '],[' ', ' ']]
+    board = Board.new(grid)
+    expect(board.grid).to eq(grid)
   end
+
+
+
+  describe '#human_to_grid' do
+  end
+
+  describe '#set_cell' do
+    it 'updates cell'
+    it 'does not write over existing value'
+  end
+
+  describe '#get_cell' do
+    it 'returns cell value'
+  end
+
 end
