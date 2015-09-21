3ea99f5966292fa438452ccd5206407f75ea4b0d
Jonny Adshead <jonny.adshead@gmail.com>
2015-09-21 14:06:41 +0100

    WIP: Basic computer response retuns rand number

--- lib/computer_player.rb
+++ lib/computer_player.rb
@@ -2,6 +2,11 @@ require_relative 'player'
 
 class ComputerPlayer < Player
 
+  def get_move
+    puts "#{@name}'s turn"
+    rand(1..9)
+  end
+
   private
 
   def get_name
--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -11,4 +11,10 @@ describe ComputerPlayer do
   it 'has own default name' do
     expect(computer.name).to eq('CP-T^3')
   end
+
+  describe '#get_move' do
+    it 'returns value for cell'  do
+      expect(computer.get_move).to be_between(1, 9)
+    end
+  end
 end
