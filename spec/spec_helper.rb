5b6d1236f43a6ec9f5ba5da87cf5152411c0b789
Jonny Adshead <jonny.adshead@gmail.com>
2015-09-22 18:03:45 +0100

    removed rspec warnings as pry very noisy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
-  config.warnings = true
+  # config.warnings = true
 
   if config.files_to_run.one?
     config.default_formatter = 'doc'
