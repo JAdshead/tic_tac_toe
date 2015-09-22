f2bf4a8f3925327616e1472ec17a8dacc80fc0f0
Jonny Adshead <jonny.adshead@gmail.com>
2015-09-22 14:35:52 +0100

    corrected typo in winning lines

--- lib/tic_tac_toe/computer_player.rb
+++ lib/tic_tac_toe/computer_player.rb
@@ -54,7 +54,7 @@ class ComputerPlayer < Player
 
   private
   def winning_lines
-    [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,9]]
+    [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]
   end
 
   # def lines_still_possible
