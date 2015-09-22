557f0db0c7976956249dc8790fc77f440cedfd67
Jonny Adshead <jonny.adshead@gmail.com>
2015-09-22 11:36:24 +0100

    computer ai will go for the win, needs refactor

--- lib/tic_tac_toe/computer_player.rb
+++ lib/tic_tac_toe/computer_player.rb
@@ -3,11 +3,37 @@ require_relative 'player'
 class ComputerPlayer < Player
 
   def get_move
-    available_moves.sample
+    possible_winning_moves.length > 0 ? possible_winning_moves.sample : available_moves.sample
   end
 
   def available_moves
     @board.free_cells
+    # [2,4,5,6,7,9]
   end
 
+  def own_moves
+    @board.find_cells @marker
+    # [1,3]
+  end
+
+  def possible_winning_moves
+    winning_moves = winning_line_numbers.map do |line|
+      arr = line - own_moves
+      if arr.length == 1
+        available_moves.include?(arr.first) ? arr.first : nil
+      else
+        nil
+      end
+    end
+
+    winning_moves.compact
+  end
+
+  def opponent_moves
+    (1..9).to_a - (own_moves + available_moves)
+  end
+
+  def winning_line_numbers
+    [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,9]]
+  end
 end
--- spec/computer_player_spec.rb
+++ spec/computer_player_spec.rb
@@ -18,20 +18,44 @@ describe ComputerPlayer do
       expect(computer.get_move).to be_between(1, 9)
     end
 
-    it 'does not return taken a value for taken cell' do
-      allow(board).to receive(:grid) { [['x','o','x'],[' ','o','x'],['x','o','x']] }
-
+    it 'does not return a value for taken cell' do
+      allow(board).to receive(:free_cells) { [4]}
       computer = ComputerPlayer.new('x',board)
       expect(computer.get_move).to eq(4)
     end
 
-    xit 'has killer instict' do
+    it 'has killer instict' do
+      allow(board).to receive(:free_cells) {[2,4,5,6,7,8,9]}
+      allow(board).to receive(:find_cells).with('x') {[1,3]}
+      computer = ComputerPlayer.new('x',board)
+
+      # run get_move 6times to minimize chance causing pass
+      results = []
+      6.times { results << computer.get_move }
+
+      expect(results).to eq([2,2,2,2,2,2])
     end
 
     xit 'will defend' do
+      allow(board).to receive(:free_cells) {[2,4,5,6,7,9]}
+      allow(board).to receive(:find_cells).with('o') {[8]}
+      computer = ComputerPlayer.new('o',board)
+
+      results = []
+      6.times { results << computer.get_move }
+
+      expect(results).to eq([2,2,2,2,2,2])
     end
 
     xit 'it will fork - create two oppotunities to win' do
+      allow(board).to receive(:free_cells) {[1,3,4,7,9]}
+      allow(board).to receive(:find_cells).with('x') {[6,8]}
+      computer = ComputerPlayer.new('x',board)
+
+      results = []
+      6.times { results << computer.get_move }
+
+      expect(results).to eq([9,9,9,9,9,9])
     end
 
     xit 'it block fork'
@@ -43,7 +67,16 @@ describe ComputerPlayer do
     xit 'it will pick empty corner'
 
     xit 'it will pick empty side'
+  end
 
+  describe '#opponent_moves' do
+    it 'returns moves made by opponent' do
+      allow(board).to receive(:free_cells) {[1,3,4,7,9]}
+      allow(board).to receive(:find_cells).with('x') {[6,8]}
+      computer = ComputerPlayer.new('x',board)
+
+      expect(computer.opponent_moves).to eq([2,5])
+    end
   end
 
 end
