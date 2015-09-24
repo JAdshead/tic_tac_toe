b7e81a079e0115e05c76262084e717b3b5ce94be
Jonny Adshead <jonny.adshead@gmail.com>
2015-09-24 15:32:24 +0100

    moved contents of force_block to own method

--- lib/tic_tac_toe/computer_player.rb
+++ lib/tic_tac_toe/computer_player.rb
@@ -3,6 +3,10 @@ require_relative 'player'
 module TicTacToe
   class ComputerPlayer < Player
 
+    WINNING_LINES = [
+      [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]
+    ]
+
     def move
       move = win || block || fork_move || force_block || block_fork
       return move if move
@@ -18,42 +22,32 @@ module TicTacToe
       board.free_cells
     end
 
-    def own_moves
+    def moves_made
       board.find_cells(marker)
     end
 
     def opponent_moves
-      (1..9).to_a - (own_moves + available_moves)
+      (1..9).to_a - (moves_made + available_moves)
     end
 
     def win
-      check_wining_moves(own_moves).sample
+      wining_moves(moves_made).sample
     end
 
     def block
-      check_wining_moves(opponent_moves).sample
-    end
-
-    def force_block
-      winning_lines.map do |line|
-        next unless (line & opponent_moves).empty? && (line & own_moves).any?
-
-        moves = line - own_moves
-
-        moves.map do |move|
-          forced_move = (line - (own_moves << move)).first
-          opponent_future_moves = opponent_moves << forced_move
-          check_wining_moves(opponent_future_moves).size < 2 ? move : nil
-        end
-      end.flatten.compact.sample
+      wining_moves(opponent_moves).sample
     end
 
     def fork_move
-      check_fork_moves(own_moves).sample
+      fork_moves(moves_made).sample
+    end
+
+    def force_block
+      force_block_moves
     end
 
     def block_fork
-      check_fork_moves(opponent_moves).sample
+      fork_moves(opponent_moves).sample
     end
 
     def go_center
@@ -63,16 +57,10 @@ module TicTacToe
     private
 
     def winning_lines
-      [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+      @winning_lines ||= WINNING_LINES
     end
 
-    # def lines_still_possible
-    #   winning_lines.select do |line|
-    #     (line - opponent_moves).size > 3
-    #   end
-    # end
-
-    def check_wining_moves(moves_made)
+    def wining_moves(moves_made)
       winning_lines.map do |line|
         arr = line - moves_made
 
@@ -82,14 +70,33 @@ module TicTacToe
       end.compact
     end
 
-    def check_fork_moves(moves_made, moves_left = available_moves)
+    def fork_moves(moves_made, moves_left = available_moves)
       moves_left.map do |move|
-        future_moves = moves_made.dup
+        future_moves = moves_made.dup << move
 
-        future_moves << move
-
-        (check_wining_moves future_moves).size > 1 ? move : nil
+        (wining_moves future_moves).size > 1 ? move : nil
       end.compact
     end
+
+    def force_block_moves
+      winning_lines.map do |line|
+        next unless (line & opponent_moves).empty? && (line & moves_made).any?
+
+        moves = line - moves_made
+
+        safe_moves(moves, line)
+      end.flatten.compact.sample
+    end
+
+    def safe_moves(moves, line)
+      moves.map do |move|
+        forced_move = (line - (moves_made << move)).first
+
+        opponent_future_moves = opponent_moves << forced_move
+
+        wining_moves(opponent_future_moves).size < 2 ? move : nil
+      end
+    end
+
   end
 end
